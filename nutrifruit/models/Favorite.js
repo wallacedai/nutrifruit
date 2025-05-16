8f69df37adeed428e320272b5b204790733f07cc
wallace <wallacedai05@gmail.com>
2025-05-15 21:43:41 -0400

    add nodemodules

--- nutrifruit/models/Favorite.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const favoriteSchema = new mongoose.Schema({
-  name: String,
-  user: String
-});
-
-module.exports = mongoose.model('Favorite', favoriteSchema);
